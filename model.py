13b8f3df587df14704f7b9bc9503b73eafd0c527
Yichuan <1758783038@qq.com>
2024-10-03 17:26:15 -0700

    Commit and Push

--- model.py
+++ model.py
@@ -1,131 +1,200 @@
-from transformers import AutoTokenizer
+import torch.nn as nn
 import torch
-from torch import nn
-from transformers import EsmModel
-from util import printl
-import numpy as np
-from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
+import torch.nn.functional as F
+import esm
+import esm_adapterH
+from peft import PeftModel, LoraConfig, get_peft_model
+
+import numpy as np  # for lora
 
 
-def get_tokenizer(configs):
-    if configs.model_scheme.lower() == 'esm2':
-        tokenizer = AutoTokenizer.from_pretrained(configs.encoder_name)
-    else:
-        raise ValueError("Wrong model_scheme specified for get_tokenizer.")
-    return tokenizer
+def load_and_add_lora_checkpoint(base_model, lora_checkpoint_path):
+    """Add a pretrained LoRa checkpoint to a base model"""
+    lora_model = PeftModel.from_pretrained(base_model, lora_checkpoint_path)
+    return lora_model
 
 
-def get_model(configs, log_path):
-    if configs.model_scheme.lower() == 'esm2':
-        encoder = Encoder(configs=configs, log_path=log_path)
-    else:
-        raise ValueError("Wrong model_scheme specified for get_model")
-    return encoder
+class ESM2(nn.Module):  # embedding table is fixed
+    def __init__(self, esm2_pretrain, logging,
+                 accelerator,
+                 configs,
+                 residue_inner_dim=4096,
+                 residue_out_dim=256,
+                 protein_out_dim=256,
+                 residue_num_projector=2,
+                 protein_inner_dim=4096, protein_num_projector=2):
+        """
+        unfix_last_layer: the number of layers that can be fine-tuned
+        """
+        super(ESM2, self).__init__()
+        if configs.model.esm_encoder.adapter_h.enable:
+            if accelerator.is_main_process:
+                logging.info("use adapter H")
+            # num_end_adapter_layers = configs.model.esm_encoder.adapter_h.num_end_adapter_layers
+            adapter_args = configs.model.esm_encoder.adapter_h
+            esm2_dict = {
+                         "esm2_t36_3B_UR50D": esm_adapterH.pretrained.esm2_t36_3B_UR50D(adapter_args),
+                         # 36 layers embedding = 2560
+                         "esm2_t33_650M_UR50D": esm_adapterH.pretrained.esm2_t33_650M_UR50D(adapter_args),
+                         # 33 layers embedding = 1280
+                         "esm2_t30_150M_UR50D": esm_adapterH.pretrained.esm2_t30_150M_UR50D(adapter_args),
+                         # 30 layers embedding = 640
+                         "esm2_t12_35M_UR50D": esm_adapterH.pretrained.esm2_t12_35M_UR50D(adapter_args),
+                         # 12 layers embedding = 480
+                         "esm2_t6_8M_UR50D": esm_adapterH.pretrained.esm2_t6_8M_UR50D(adapter_args),
+                         # 6 layers embedding = 320
+                         }
+        else:
+            esm2_dict = {
+                         "esm2_t36_3B_UR50D": esm.pretrained.esm2_t36_3B_UR50D(),
+                         # 36 layers embedding = 2560
+                         "esm2_t33_650M_UR50D": esm.pretrained.esm2_t33_650M_UR50D(),
+                         # 33 layers embedding = 1280
+                         "esm2_t30_150M_UR50D": esm.pretrained.esm2_t30_150M_UR50D(),
+                         # 30 layers embedding = 640
+                         "esm2_t12_35M_UR50D": esm.pretrained.esm2_t12_35M_UR50D(),
+                         # 12 layers embedding = 480
+                         "esm2_t6_8M_UR50D": esm.pretrained.esm2_t6_8M_UR50D(),
+                         # 6 layers embedding = 320
+                         }
 
+        # if esm2_pretrain_local is None:
+        self.esm2, self.alphabet = esm2_dict[esm2_pretrain]  # alphabet.all_toks
+        # else:
+        #     print("load esm2 model from local dir")
+        #     self.esm2, self.alphabet = esm.pretrained.load_model_and_alphabet_local(esm2_pretrain_local)
 
-def get_esm(configs, log_path):
-    model = EsmModel.from_pretrained(configs.encoder_name)
-    # Freeze all layers
-    for param in model.parameters():
-        param.requires_grad = False
-    if configs.training_mode.lower() == "frozen":
-        # Freeze all layers
-        for param in model.parameters():
-            param.requires_grad = False
-        printl("All model parameters have been frozen.", log_path=log_path)
-    elif configs.training_mode.lower() == "finetune":
-        # Allow the parameters of the last transformer block to be updated during fine-tuning
-        for param in model.encoder.layer[configs.finetune_layer:].parameters():
-            param.requires_grad = True
-        printl(f"Parameters in the last {0 - configs.finetune_layer} layers are trainable.", log_path=log_path)
-        for param in model.pooler.parameters():
-            param.requires_grad = False
-        printl("Pooling layer parameters have been frozen.", log_path=log_path)
-    else:
-        raise ValueError("Wrong training_mode specified for get_esm.")
-    return model
+        self.num_layers = self.esm2.num_layers
+        for p in self.esm2.parameters():  # frozen all parameters first
+            p.requires_grad = False
 
+        if configs.model.esm_encoder.adapter_h.enable:
+            for name, param in self.esm2.named_parameters():
+                if "adapter_layer" in name:
+                    # print("unfix adapter_layer")
+                    param.requires_grad = True
 
-def load_opt(model, config):
-    scheduler = None
-    if config.optimizer.name.lower() == 'adam':
-        # opt = eval('torch.optim.' + config.optimizer.name)(model.parameters(), lr=config.optimizer.lr, eps=eps,
-        #                                       weight_decay=config.optimizer.weight_decay)
-        opt = torch.optim.AdamW(
-            model.parameters(), lr=float(config.optimizer.lr),
-            betas=(config.optimizer.beta_1, config.optimizer.beta_2),
-            weight_decay=float(config.optimizer.weight_decay),
-            eps=float(config.optimizer.eps)
-        )
-    elif config.optimizer.name.lower() == 'sgd':
-        opt = torch.optim.SGD(model.parameters(), lr=float(config.optimizer.lr),
-                              momentum=0.9, dampening=0,
-                              weight_decay=float(config.optimizer.weight_decay))
-
-    else:
-        raise ValueError('wrong optimizer')
-    return opt, scheduler
-
-
-def get_optimizer(net, configs, num_train_samples, logfilepath):
-    optimizer, scheduler = load_opt(net, configs)
-    if configs.optimizer.mode == "skip":
-        scheduler = None
-    else:
-        if scheduler is None:
-            if configs.optimizer.decay.first_cycle_steps:
-                first_cycle_steps = configs.optimizer.decay.first_cycle_steps
+        if configs.model.esm_encoder.lora.enable:
+            if accelerator.is_main_process:
+                logging.info('use lora for esm v2')
+            if configs.model.esm_encoder.lora.resume.enable:
+                self.esm2 = load_and_add_lora_checkpoint(self.esm2, configs.model.esm_encoder.lora.resume)
             else:
-                first_cycle_steps=np.ceil(
-                    num_train_samples / configs.train_settings.grad_accumulation) * configs.train_settings.num_epochs / configs.optimizer.decay.num_restarts
-            print("first_cycle_steps="+str(first_cycle_steps))
-            scheduler = CosineAnnealingWarmupRestarts(
-                optimizer,
-                first_cycle_steps=first_cycle_steps,
-                cycle_mult=1.0,
-                max_lr=configs.optimizer.lr,
-                min_lr=configs.optimizer.decay.min_lr,
-                warmup_steps=configs.optimizer.decay.warmup,
-                gamma=configs.optimizer.decay.gamma)
-    return optimizer, scheduler
+                lora_targets = ["self_attn.q_proj", "self_attn.k_proj", "self_attn.v_proj", "self_attn.out_proj"]
+                target_modules = []
+                if configs.model.esm_encoder.lora.esm_num_end_lora > 0:
+                    start_layer_idx = np.max([self.num_layers - configs.model.esm_encoder.lora.esm_num_end_lora, 0])
+                    for idx in range(start_layer_idx, self.num_layers):
+                        for layer_name in lora_targets:
+                            target_modules.append(f"layers.{idx}.{layer_name}")
+                    
+                peft_config = LoraConfig(
+                    inference_mode=False,
+                    r=configs.model.esm_encoder.lora.r,
+                    lora_alpha=configs.model.esm_encoder.lora.alpha,
+                    target_modules=target_modules,
+                    lora_dropout=configs.model.esm_encoder.lora.dropout,
+                    bias="none",
+                    # modules_to_save=modules_to_save
+                )
+                self.peft_model = get_peft_model(self.esm2, peft_config)
+        elif configs.model.esm_encoder.fine_tuning.enable:
+            if accelerator.is_main_process:
+                logging.info('fine-tune esm v2')
+            unfix_last_layer = configs.model.esm_encoder.fine_tuning.unfix_last_layer
+            fix_layer_num = self.num_layers - unfix_last_layer
+            fix_layer_index = 0
+            for layer in self.esm2.layers:  # only fine-tune transformer layers,no contact_head and other parameters
+                if fix_layer_index < fix_layer_num:
+                    fix_layer_index += 1  # keep these layers frozen
+                    continue
+
+                for p in layer.parameters():
+                    # logging.info('unfix layer')
+                    p.requires_grad = True
+
+            if unfix_last_layer != 0:  # if need fine-tune last layer, the emb_layer_norm_after for last representation should updated
+                for p in self.esm2.emb_layer_norm_after.parameters():
+                    p.requires_grad = True
+
+        if configs.model.esm_encoder.tune_ESM_table:
+            if accelerator.is_main_process:
+                logging.info("fine-tune esm embedding parameters")
+            for p in self.esm2.embed_tokens.parameters():
+                p.requires_grad = True
+        
+        if hasattr(configs.model.esm_encoder, "MLM"):
+            if configs.model.esm_encoder.MLM.enable and configs.model.esm_encoder.MLM.mode == "predict":
+                for p in self.esm2.lm_head.parameters():
+                    p.requires_grad = True
+
+        self.pool_mode = configs.model.esm_encoder.pool_mode
+
+        self.projectors_residue = MoBYMLP(in_dim=self.esm2.embed_dim,
+                                          inner_dim=residue_inner_dim,
+                                          num_layers=residue_num_projector,
+                                          out_dim=residue_out_dim)
+
+        self.projectors_protein = MoBYMLP(in_dim=self.esm2.embed_dim,
+                                          inner_dim=protein_inner_dim,
+                                          num_layers=protein_num_projector,
+                                          out_dim=protein_out_dim)
+
+    def forward(self, x, return_logits=False, return_embedding=False):
+        outputs = self.esm2(x, repr_layers=[self.num_layers], return_contacts=False)
+        # print("OKOKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK")
+        if return_logits:
+            prediction_scores = outputs["logits"]
+            return prediction_scores
+        else:
+            residue_feature = outputs['representations'][self.num_layers]
+            # residue_dim = residue_feature.shape #[batch,L,D]
+            # average pooling but remove padding tokens
+            mask = (x != self.alphabet.padding_idx)  # use this in v2 training
+            denom = torch.sum(mask, -1, keepdim=True)
+            graph_feature_embedding = torch.sum(residue_feature * mask.unsqueeze(-1), dim=1) / denom  # remove padding
+            # print("size of graph_feature_embedding is :")
+            # print(len(graph_feature_embedding))
+            graph_feature = self.projectors_protein(graph_feature_embedding)  # the cls and eos token is included
+            mask = ((x != self.alphabet.padding_idx) & (x != self.alphabet.cls_idx) & (
+                    x != self.alphabet.eos_idx))  # use this in v2 training
+            residue_feature_embedding = residue_feature[mask]
+            residue_feature = self.projectors_residue(residue_feature_embedding)
+            # residue_feature = self.projectors_residue(residue_feature.view(-1, residue_dim[-1])).view(residue_dim[0],residue_dim[1],-1)
+            if return_embedding:
+                return graph_feature, residue_feature, graph_feature_embedding, residue_feature_embedding
+            else:
+                return graph_feature, residue_feature
 
 
-class Encoder(nn.Module):
-    def __init__(self, configs, log_path):
-        super().__init__()
-        self.model = get_esm(configs, log_path)
-        # self.pooling_layer = nn.AdaptiveAvgPool2d((None, 1))
-        # self.pooling_layer = nn.AdaptiveAvgPool1d(1)
+def prepare_models(logging, configs, accelerator):
+    # Use ESM2 for sequence
+    model_seq = ESM2(configs.model.esm_encoder.model_name,
+                     accelerator=accelerator,
+                     residue_inner_dim=configs.model.esm_encoder.residue_inner_dim,
+                     residue_out_dim=configs.model.residue_out_dim,
+                     protein_out_dim=configs.model.protein_out_dim,
+                     residue_num_projector=configs.model.residue_num_projector,
+                     protein_num_projector=configs.model.protein_num_projector,
+                     configs=configs, logging=logging)
+    
+    
+    if hasattr(configs.train_settings,"train_lm_head_only"):
+            if configs.train_settings.train_lm_head_only is True:
+                for name, param in model_struct.named_parameters():
+                    param.requires_grad = False
+                for name, param in model_seq.named_parameters():
+                    param.requires_grad = False
+                for p in model_seq.esm2.lm_head.parameters():
+                        p.requires_grad = True #will open esm2.embed_tokens.weight!
+    
+    if accelerator.is_main_process:
+        print_trainable_parameters(model_seq, logging)
+        print_trainable_parameters(model_struct, logging)
 
-    def forward(self, x):
-        return x
+    simclr = SimCLR(model_seq, model_struct, configs=configs)
+    return simclr  # model_seq, model_struct #, new_model
 
 
-class LayerNormNet(nn.Module):
-    """
-    This implementation was inspired by https://github.com/tttianhao/CLEAN
-    """
-    def __init__(self, configs, hidden_dim=512, out_dim=256):
-        super(LayerNormNet, self).__init__()
-        self.hidden_dim1 = hidden_dim
-        self.out_dim = out_dim
-        self.drop_out = configs.supcon.drop_out
-        self.device = configs.train_settings.device
-        self.dtype = torch.float32
-        feature_dim = {"facebook/esm2_t6_8M_UR50D": 320, "facebook/esm2_t33_650M_UR50D": 1280,
-                       "facebook/esm2_t30_150M_UR50D": 640, "facebook/esm2_t12_35M_UR50D": 480}
-        self.fc1 = nn.Linear(feature_dim[configs.encoder.model_name], hidden_dim, dtype=self.dtype, device=self.device)
-        self.ln1 = nn.LayerNorm(hidden_dim, dtype=self.dtype, device=self.device)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim,
-                             dtype=self.dtype, device=self.device)
-        self.ln2 = nn.LayerNorm(hidden_dim, dtype=self.dtype, device=self.device)
-        self.fc3 = nn.Linear(hidden_dim, out_dim, dtype=self.dtype, device=self.device)
-        self.dropout = nn.Dropout(p=self.drop_out)
-
-    def forward(self, x):
-        x = self.dropout(self.ln1(self.fc1(x)))
-        x = torch.relu(x)
-        x = self.dropout(self.ln2(self.fc2(x)))
-        x = torch.relu(x)
-        x = self.fc3(x)
-        return x
+if __name__ == '__main__':
+    print('test')
