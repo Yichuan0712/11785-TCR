fba65d567ca7e986270eb22a1038f01d1e5fbe29
Yichuan <1758783038@qq.com>
2024-10-12 22:36:49 -0700

    Commit and Push

--- run.py
+++ run.py
@@ -62,8 +62,8 @@ def main(parse_args, configs):
     """
     printl(f"{'=' * 128}", log_path=log_path)
     dataloaders_dict = get_dataloader(configs)
-    printl(f'Number of Steps for Training Data: {len(dataloaders_dict["train_loader"])}', log_path=log_path)
-    printl(f'Number of Steps for Validation Data: {len(dataloaders_dict["valid_loader"])}', log_path=log_path)
+    # printl(f'Number of Steps for Training Data: {len(dataloaders_dict["train_loader"])}', log_path=log_path)
+    # printl(f'Number of Steps for Validation Data: {len(dataloaders_dict["valid_loader"])}', log_path=log_path)
     # printl(f'Number of Steps for Test Data: {len(dataloaders_dict["test"])}', log_path=log_path)
     printl("Data loading complete.", log_path=log_path)
     """
