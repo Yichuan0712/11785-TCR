b4f5754d0cff2450a15d21d88b0d297834ab9176
Yichuan <1758783038@qq.com>
2024-10-16 22:33:48 -0700

    Commit and Push

--- run.py
+++ run.py
@@ -248,6 +248,8 @@ def train_multi(encoder, projection_head, epoch, train_loader, tokenizer, optimi
         raise ValueError("Invalid batch mode specified in configs.")
 
     for batch, data in progress_bar:
+        print(data)
+        exit(0)
         epitope_list = data['anchor_epitope']
         anchor_positive_negative_list = data['anchor_positive_negative_TCR']
 
