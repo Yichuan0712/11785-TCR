b5cd1a6740201bd2f000a625e73e7f5a71e766fb
Yichuan <1758783038@qq.com>
2024-10-16 23:42:23 -0700

    Commit and Push

--- run.py
+++ run.py
@@ -262,8 +262,8 @@ def train_multi(encoder, projection_head, epoch, train_loader, tokenizer, optimi
             # exit(0)
             anchor_positive_negative_list.append(anc_pos_neg_emb_mini_batch)
         anchor_positive_negative = torch.stack(anchor_positive_negative_list)
-        # print(anchor_positive_negative.shape)
-        # exit(0)
+        print(anchor_positive_negative.shape)
+        exit(0)
 
         loss = criterion(anchor_positive_negative, configs.temp, configs.n_pos)
 
