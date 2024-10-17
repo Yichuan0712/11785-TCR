edaf1ea113d2ce425cd34947a1e53a8356b12002
Yichuan <1758783038@qq.com>
2024-10-16 22:51:34 -0700

    Commit and Push

--- run.py
+++ run.py
@@ -254,8 +254,10 @@ def train_multi(encoder, projection_head, epoch, train_loader, tokenizer, optimi
             epitope_list.append(element['anchor_epitope'])
             anc_pos_neg_mini_batch = [(None, str(element['anchor_positive_negative_TCR'][i])) for i in range(len(element['anchor_positive_negative_TCR']))]
             print(anc_pos_neg_mini_batch)
-            _, _, anc_pos_neg_tokens = tokenizer(anc_pos_neg_mini_batch)
-            print(anc_pos_neg_tokens)
+            _, _, anc_pos_neg_tokens_mini_batch = tokenizer(anc_pos_neg_mini_batch)
+            print(anc_pos_neg_tokens_mini_batch)
+            anc_pos_neg_emb_mini_batch = projection_head(encoder(anc_pos_neg_tokens_mini_batch.to(device)).mean(dim=1))
+            print(anc_pos_neg_emb_mini_batch)
             exit(0)
 
         print(data)
