6ae525a5bfc5a7aa76f94ac21b4bcd8c6c55ef7f
Yichuan <1758783038@qq.com>
2024-10-28 22:29:16 -0700

    Commit and Push

--- run.py
+++ run.py
@@ -166,16 +166,14 @@ def main(parse_args, configs):
         true_classes, predicted_classes = infer_one(encoder, projection_head, inference_dataloaders["train_loader"], tokenizer, inference_dataloaders["test_loader"], log_path)
         print(true_classes)
         print(predicted_classes)
-        # correct_predictions = sum(1 for true, pred in zip(true_classes, predicted_classes) if true == pred)
-        # accuracy = correct_predictions / len(true_classes) if len(true_classes) > 0 else 0
-        # print(f"Accuracy: {accuracy:.4f}")  # better metrics are needed
+
         from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
         from sklearn.preprocessing import LabelEncoder
         # Instantiate a label encoder
         label_encoder = LabelEncoder()
-
+        all_classes = list(set(true_classes) | set(predicted_classes))
         # Encode string labels to numeric values
-        true_encoded = label_encoder.fit_transform(true_classes)
+        true_encoded = label_encoder.fit_transform(all_classes)
         predicted_encoded = label_encoder.transform(predicted_classes)
 
         # Calculate accuracy
