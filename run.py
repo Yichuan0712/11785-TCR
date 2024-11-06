e17bd136b43064e1430443f084c1775c195f31c8
Yichuan <1758783038@qq.com>
2024-11-06 15:26:14 -0800

    Commit and Push

--- run.py
+++ run.py
@@ -161,87 +161,87 @@ def main(parse_args, configs):
         scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
         printl("Tokenizer, Optimizer and Scheduler successfully resumed from checkpoint.", log_path=log_path)
 
-    # elif parse_args.mode == 'predict' and parse_args.resume_path is not None:
-    #     printl("Start prediction.", log_path=log_path)
-    #     printl(f"{'=' * 128}", log_path=log_path)
-    #     true_classes, predicted_classes, predicted_scores = infer_one(encoder, projection_head, inference_dataloaders["train_loader"], tokenizer, inference_dataloaders["test_loader"], log_path)
-    #     print(true_classes)
-    #     print(predicted_classes)
-    #
-    #     correct_predictions = sum(1 for true, pred in zip(true_classes, predicted_classes) if true == pred)
-    #     accuracy = correct_predictions / len(true_classes) if len(true_classes) > 0 else 0
-    #
-    #     # 获取所有唯一类别
-    #     unique_classes = set(true_classes + predicted_classes)
-    #
-    #     # 初始化 Precision、Recall 和 F1 Score 列表
-    #     precision_scores = []
-    #     recall_scores = []
-    #     f1_scores = []
-    #
-    #     for label in unique_classes:
-    #         # 计算每个类别的 True Positives、False Positives 和 False Negatives
-    #         true_positive = sum(
-    #             1 for true, pred in zip(true_classes, predicted_classes) if true == label and pred == label)
-    #         predicted_positive = sum(1 for pred in predicted_classes if pred == label)
-    #         actual_positive = sum(1 for true in true_classes if true == label)
-    #
-    #         # 计算 Precision
-    #         precision = true_positive / predicted_positive if predicted_positive > 0 else 0
-    #         precision_scores.append(precision)
-    #
-    #         # 计算 Recall
-    #         recall = true_positive / actual_positive if actual_positive > 0 else 0
-    #         recall_scores.append(recall)
-    #
-    #         # 计算 F1 Score
-    #         f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-    #         f1_scores.append(f1)
-    #
-    #     precision = sum(precision_scores) / len(precision_scores)
-    #     recall = sum(recall_scores) / len(recall_scores)
-    #     f1 = sum(f1_scores) / len(f1_scores)
-    #
-    #     lb = LabelBinarizer()
-    #     true_binarized = lb.fit_transform(true_classes)
-    #
-    #     # 提取所有类别
-    #     all_classes = sorted(set(true_classes + predicted_classes))
-    #
-    #     # 将 predicted_scores 转换为二维数组，顺序匹配 all_classes
-    #     predicted_scores_array = np.array([
-    #         [score_dict.get(cls, 0.0) for cls in all_classes]
-    #         for score_dict in predicted_scores
-    #     ])
-    #
-    #     # 检查 true_binarized 和 predicted_scores_array 的每一列，移除那些没有正负样本的类别
-    #     valid_indices = []
-    #     for i in range(true_binarized.shape[1]):
-    #         if len(np.unique(true_binarized[:, i])) > 1:  # 确保每个类别至少有一个正样本和一个负样本
-    #             valid_indices.append(i)
-    #
-    #     # 对 true_binarized 和 predicted_scores_array 进行筛选
-    #     true_binarized_filtered = true_binarized[:, valid_indices]
-    #     predicted_scores_array_filtered = predicted_scores_array[:, valid_indices]
-    #
-    #     # 计算 AUC
-    #     auc = roc_auc_score(true_binarized_filtered, predicted_scores_array_filtered, average="macro",
-    #                         multi_class="ovr")
-    #
-    #     # 显示各项指标
-    #     print(f"Accuracy: {accuracy:.4f}")
-    #     print(f"Precision: {precision:.4f}")
-    #     print(f"Recall: {recall:.4f}")
-    #     print(f"F1 Score: {f1:.4f}")
-    #     print(f"AUC: {auc:.4f}" if auc is not None else "AUC: Not applicable")
-    #     return
     elif parse_args.mode == 'predict' and parse_args.resume_path is not None:
         printl("Start prediction.", log_path=log_path)
         printl(f"{'=' * 128}", log_path=log_path)
+        true_classes, predicted_classes, predicted_scores = infer_one(encoder, projection_head, dataloaders["train_loader"], tokenizer, inference_dataloaders["test_loader"], log_path)
+        print(true_classes)
+        print(predicted_classes)
 
-        infer_features(encoder, projection_head, inference_dataloaders["train_loader"], tokenizer, inference_dataloaders["test_loader"], log_path)
+        correct_predictions = sum(1 for true, pred in zip(true_classes, predicted_classes) if true == pred)
+        accuracy = correct_predictions / len(true_classes) if len(true_classes) > 0 else 0
 
+        # 获取所有唯一类别
+        unique_classes = set(true_classes + predicted_classes)
+
+        # 初始化 Precision、Recall 和 F1 Score 列表
+        precision_scores = []
+        recall_scores = []
+        f1_scores = []
+
+        for label in unique_classes:
+            # 计算每个类别的 True Positives、False Positives 和 False Negatives
+            true_positive = sum(
+                1 for true, pred in zip(true_classes, predicted_classes) if true == label and pred == label)
+            predicted_positive = sum(1 for pred in predicted_classes if pred == label)
+            actual_positive = sum(1 for true in true_classes if true == label)
+
+            # 计算 Precision
+            precision = true_positive / predicted_positive if predicted_positive > 0 else 0
+            precision_scores.append(precision)
+
+            # 计算 Recall
+            recall = true_positive / actual_positive if actual_positive > 0 else 0
+            recall_scores.append(recall)
+
+            # 计算 F1 Score
+            f1 = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+            f1_scores.append(f1)
+
+        precision = sum(precision_scores) / len(precision_scores)
+        recall = sum(recall_scores) / len(recall_scores)
+        f1 = sum(f1_scores) / len(f1_scores)
+
+        lb = LabelBinarizer()
+        true_binarized = lb.fit_transform(true_classes)
+
+        # 提取所有类别
+        all_classes = sorted(set(true_classes + predicted_classes))
+
+        # 将 predicted_scores 转换为二维数组，顺序匹配 all_classes
+        predicted_scores_array = np.array([
+            [score_dict.get(cls, 0.0) for cls in all_classes]
+            for score_dict in predicted_scores
+        ])
+
+        # 检查 true_binarized 和 predicted_scores_array 的每一列，移除那些没有正负样本的类别
+        valid_indices = []
+        for i in range(true_binarized.shape[1]):
+            if len(np.unique(true_binarized[:, i])) > 1:  # 确保每个类别至少有一个正样本和一个负样本
+                valid_indices.append(i)
+
+        # 对 true_binarized 和 predicted_scores_array 进行筛选
+        true_binarized_filtered = true_binarized[:, valid_indices]
+        predicted_scores_array_filtered = predicted_scores_array[:, valid_indices]
+
+        # 计算 AUC
+        auc = roc_auc_score(true_binarized_filtered, predicted_scores_array_filtered, average="macro",
+                            multi_class="ovr")
+
+        # 显示各项指标
+        print(f"Accuracy: {accuracy:.4f}")
+        print(f"Precision: {precision:.4f}")
+        print(f"Recall: {recall:.4f}")
+        print(f"F1 Score: {f1:.4f}")
+        print(f"AUC: {auc:.4f}" if auc is not None else "AUC: Not applicable")
         return
+    # elif parse_args.mode == 'predict' and parse_args.resume_path is not None:
+    #     printl("Start prediction.", log_path=log_path)
+    #     printl(f"{'=' * 128}", log_path=log_path)
+    #
+    #     infer_features(encoder, projection_head, inference_dataloaders["train_loader"], tokenizer, inference_dataloaders["test_loader"], log_path)
+    #
+    #     return
     else:
         raise NotImplementedError
 
@@ -504,88 +504,88 @@ def train_multi(encoder, projection_head, epoch, train_loader, tokenizer, optimi
     return None
 
 
-# def infer_one(encoder, projection_head, train_loader, tokenizer, valid_or_test_loader, log_path):
-#     device = torch.device("cuda")
-#     # Set models to evaluation mode for inference
-#     encoder.eval()
-#     projection_head.eval()
-#
-#     progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Cluster Center Calculation")
-#
-#     log_dir = os.path.dirname(log_path)
-#     log_file_average = os.path.join(log_dir, "epitope_average.pkl")
-#
-#     epitope_sums = {}
-#     epitope_counts = {}
-#
-#     with torch.no_grad():
-#         for batch, data in progress_bar:
-#             epitope_list = data['anchor_epitope']
-#             anchor_list = data['anchor_TCR']
-#
-#             anchor_seq_batch = [(epitope_list[i], str(anchor_list[i])) for i in range(len(epitope_list))]
-#             _, _, anchor_tokens = tokenizer(anchor_seq_batch)
-#
-#             anchor_embs = projection_head(encoder(anchor_tokens.to(device)).mean(dim=1))
-#
-#             for i, epitope in enumerate(epitope_list):
-#                 if epitope not in epitope_sums:
-#                     epitope_sums[epitope] = anchor_embs[i]
-#                     epitope_counts[epitope] = 1
-#                 else:
-#                     epitope_sums[epitope] += anchor_embs[i]
-#                     epitope_counts[epitope] += 1
-#     epitope_data = {
-#         epitope: {
-#             "average_embedding": (epitope_sums[epitope] / epitope_counts[epitope]),
-#             "count": epitope_counts[epitope]
-#         }
-#         for epitope in epitope_sums
-#     }
-#
-#     with open(log_file_average, "wb") as f:
-#         pickle.dump(epitope_data, f)
-#         printl(f"Cluster center calculation completed and saved to {log_file_average}.", log_path=log_path)
-#
-#     progress_bar2 = tqdm(enumerate(valid_or_test_loader), total=len(valid_or_test_loader), desc="Finding Nearest Cluster Centers")
-#     true_classes = []
-#     predicted_classes = []
-#
-#     prediction_probabilities = []
-#
-#     with torch.no_grad():
-#         for batch, data in progress_bar2:
-#             epitope_list = data['anchor_epitope']
-#             anchor_list = data['anchor_TCR']
-#
-#             anchor_seq_batch = [(epitope_list[i], str(anchor_list[i])) for i in range(len(epitope_list))]
-#             _, _, anchor_tokens = tokenizer(anchor_seq_batch)
-#
-#             anchor_embs = projection_head(encoder(anchor_tokens.to(device)).mean(dim=1))
-#
-#             for i, epitope in enumerate(epitope_list):
-#                 true_classes.append(epitope)
-#
-#                 # Calculate distances to all cluster centers and convert to probabilities
-#                 distances = []
-#                 for cluster_epitope, cluster_data in epitope_data.items():
-#                     cluster_emb = cluster_data["average_embedding"].to(device)
-#                     distance = torch.dist(anchor_embs[i], cluster_emb).item()
-#                     distances.append((cluster_epitope, distance))
-#
-#                 # Convert distances to similarity scores (e.g., inverse distance or cosine similarity)
-#                 inverse_distances = torch.tensor([1 / (d[1] + 1e-8) for d in distances])  # Avoid division by zero
-#                 probabilities = F.softmax(inverse_distances, dim=0).cpu().numpy()
-#
-#                 # Get the epitope with the highest probability as prediction
-#
-#                 nearest_epitope = distances[np.argmax(probabilities)][0]
-#                 predicted_classes.append(nearest_epitope)
-#                 prediction_probabilities.append(dict(zip([d[0] for d in distances], probabilities)))
-#
-#     return true_classes, predicted_classes, prediction_probabilities
-#
-#     # return true_classes, predicted_classes
+def infer_one(encoder, projection_head, train_loader, tokenizer, valid_or_test_loader, log_path):
+    device = torch.device("cuda")
+    # Set models to evaluation mode for inference
+    encoder.eval()
+    projection_head.eval()
+
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Cluster Center Calculation")
+
+    log_dir = os.path.dirname(log_path)
+    log_file_average = os.path.join(log_dir, "epitope_average.pkl")
+
+    epitope_sums = {}
+    epitope_counts = {}
+
+    with torch.no_grad():
+        for batch, data in progress_bar:
+            epitope_list = data['anchor_epitope']
+            anchor_list = data['anchor_TCR']
+
+            anchor_seq_batch = [(epitope_list[i], str(anchor_list[i])) for i in range(len(epitope_list))]
+            _, _, anchor_tokens = tokenizer(anchor_seq_batch)
+
+            anchor_embs = projection_head(encoder(anchor_tokens.to(device)).mean(dim=1))
+
+            for i, epitope in enumerate(epitope_list):
+                if epitope not in epitope_sums:
+                    epitope_sums[epitope] = anchor_embs[i]
+                    epitope_counts[epitope] = 1
+                else:
+                    epitope_sums[epitope] += anchor_embs[i]
+                    epitope_counts[epitope] += 1
+    epitope_data = {
+        epitope: {
+            "average_embedding": (epitope_sums[epitope] / epitope_counts[epitope]),
+            "count": epitope_counts[epitope]
+        }
+        for epitope in epitope_sums
+    }
+
+    with open(log_file_average, "wb") as f:
+        pickle.dump(epitope_data, f)
+        printl(f"Cluster center calculation completed and saved to {log_file_average}.", log_path=log_path)
+
+    progress_bar2 = tqdm(enumerate(valid_or_test_loader), total=len(valid_or_test_loader), desc="Finding Nearest Cluster Centers")
+    true_classes = []
+    predicted_classes = []
+
+    prediction_probabilities = []
+
+    with torch.no_grad():
+        for batch, data in progress_bar2:
+            epitope_list = data['anchor_epitope']
+            anchor_list = data['anchor_TCR']
+
+            anchor_seq_batch = [(epitope_list[i], str(anchor_list[i])) for i in range(len(epitope_list))]
+            _, _, anchor_tokens = tokenizer(anchor_seq_batch)
+
+            anchor_embs = projection_head(encoder(anchor_tokens.to(device)).mean(dim=1))
+
+            for i, epitope in enumerate(epitope_list):
+                true_classes.append(epitope)
+
+                # Calculate distances to all cluster centers and convert to probabilities
+                distances = []
+                for cluster_epitope, cluster_data in epitope_data.items():
+                    cluster_emb = cluster_data["average_embedding"].to(device)
+                    distance = torch.dist(anchor_embs[i], cluster_emb).item()
+                    distances.append((cluster_epitope, distance))
+
+                # Convert distances to similarity scores (e.g., inverse distance or cosine similarity)
+                inverse_distances = torch.tensor([1 / (d[1] + 1e-8) for d in distances])  # Avoid division by zero
+                probabilities = F.softmax(inverse_distances, dim=0).cpu().numpy()
+
+                # Get the epitope with the highest probability as prediction
+
+                nearest_epitope = distances[np.argmax(probabilities)][0]
+                predicted_classes.append(nearest_epitope)
+                prediction_probabilities.append(dict(zip([d[0] for d in distances], probabilities)))
+
+    return true_classes, predicted_classes, prediction_probabilities
+
+    # return true_classes, predicted_classes
 
 
 def infer_features(encoder, projection_head, train_loader, tokenizer, valid_or_test_loader, log_path):
