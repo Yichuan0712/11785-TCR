1495b3d0bbe757ee2ce568f3b005639ac527130e
Yichuan <1758783038@qq.com>
2024-11-22 15:04:32 -0800

    Commit and Push

---
+++ slurm/final/final_clean_adapter_20.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mem 50G
+#SBATCH -n 1
+#SBATCH --gres gpu:A100:1
+
+#SBATCH --error=error_final_clean_adapter_20.log
+#SBATCH --output=output_final_clean_adapter_20.log
+
+#SBATCH --time 2-00:00:00 #Time for the job to run
+#SBATCH --job-name CleAdp20
+
+##SBATCH -p requeue
+#SBATCH -p gpu
+
+##SBATCH -p xudong-gpu
+##SBATCH -A xudong-lab
+
+module load miniconda3
+
+# Activate the Conda environment
+source activate /home/yz3qt/data/miniconda/envs/splm
+
+export TORCH_HOME=/home/yz3qt/data/torch_cache/
+export HF_HOME=/home/yz3qt/data/transformers_cache/
+
+python ./run.py --config_path ./config/final/final_clean_adapter_20.yaml --mode train || true
---
+++ slurm/final/final_clean_finetune_6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mem 50G
+#SBATCH -n 1
+#SBATCH --gres gpu:A100:1
+
+#SBATCH --error=error_final_clean_finetune_6.log
+#SBATCH --output=output_final_clean_finetune_6.log
+
+#SBATCH --time 2-00:00:00 #Time for the job to run
+#SBATCH --job-name CleFt6
+
+##SBATCH -p requeue
+#SBATCH -p gpu
+
+##SBATCH -p xudong-gpu
+##SBATCH -A xudong-lab
+
+module load miniconda3
+
+# Activate the Conda environment
+source activate /home/yz3qt/data/miniconda/envs/splm
+
+export TORCH_HOME=/home/yz3qt/data/torch_cache/
+export HF_HOME=/home/yz3qt/data/transformers_cache/
+
+python ./run.py --config_path ./config/final/final_clean_finetune_6.yaml --mode train || true
