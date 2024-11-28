6aa2e63e4bc60cd53b80f8c54282543388f09334
Yichuan <1758783038@qq.com>
2024-11-27 18:05:19 -0800

    Commit and Push

--- slurm/final_extract/final_clean_extract.sh
+++ slurm/final_extract/final_clean_extract.sh
@@ -23,15 +23,15 @@ source activate /home/yz3qt/data/miniconda/envs/splm
 export TORCH_HOME=/home/yz3qt/data/torch_cache/
 export HF_HOME=/home/yz3qt/data/transformers_cache/
 
-python ./run.py --config_path ./config/final/final_clean.yaml --resume_path ./result/final_clean/20241122-18-20-14/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_adapter_12.yaml --resume_path ./result/final_clean/20241122-18-55-48/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_adapter_16.yaml --resume_path ./result/final_clean/20241122-19-10-45/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_adapter_20.yaml --resume_path ./result/final_clean/20241122-19-11-50/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_finetune_2.yaml --resume_path ./result/final_clean/20241122-19-46-37/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_finetune_4.yaml --resume_path ./result/final_clean/20241122-19-48-48/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_finetune_6.yaml --resume_path ./result/final_clean/20241122-20-03-52/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_lora_16.yaml --resume_path ./result/final_clean/20241122-20-15-22/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
-python ./run.py --config_path ./config/final/final_clean_lora_33.yaml --resume_path ./result/final_clean/20241122-22-26-07/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean.yaml --resume_path ./result/final_clean/20241122-18-14-57/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_adapter_12.yaml --resume_path ./result/final_clean/20241124-22-55-47/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_adapter_16.yaml --resume_path ./result/final_clean/20241125-01-48-58/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_adapter_20.yaml --resume_path ./result/final_clean/20241125-02-08-58/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_finetune_2.yaml --resume_path ./result/final_clean/20241125-03-52-40/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_finetune_4.yaml --resume_path ./result/final_clean/20241125-04-50-45/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_finetune_6.yaml --resume_path ./result/final_clean/20241125-07-24-02/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_lora_16.yaml --resume_path ./result/final_clean/20241125-08-36-05/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
+python ./run.py --config_path ./config/final/final_clean_lora_33.yaml --resume_path ./result/final_clean/20241125-10-04-36/checkpoint/model_triplet.pth  --mode extract --result_path ./result/final_clean_extract/ || true
 
 
 
