1e701483ad8d2f677d19c579ba63c1f39625d6d4
Yichuan <1758783038@qq.com>
2024-12-01 15:21:35 -0800

    Commit and Push

---
+++ slurm/final/final_clean_adapter_20_1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --mem 50G
+#SBATCH -n 1
+#SBATCH --gres gpu:A100:1
+
+#SBATCH --error=error_final_clean_adapter_20.log
+#SBATCH --output=output_final_clean_adapter_20.log
+
+#SBATCH --time 2-00:00:00 #Time for the job to run
+#SBATCH --job-name ClnAdp20
+
+##SBATCH -p requeue
+#SBATCH -p gpu
+
+##SBATCH -p xudong-gpu
+##SBATCH -A xudong-lab
+
+module load miniconda3
+
+# Activate the Conda environment
+source activate /home/yz3qt/data/miniconda/envs/splm
+
+export TORCH_HOME=/home/yz3qt/data/torch_cache/
+export HF_HOME=/home/yz3qt/data/transformers_cache/
+
+python ./run.py --config_path ./config/final/final_clean_adapter_20_1.yaml --mode train --result_path ./result/final_clean/ || true
