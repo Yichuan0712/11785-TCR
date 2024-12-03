6907f6a0567e8f2f2b47cd477c147dbfbcf5b4ca
Yichuan <1758783038@qq.com>
2024-12-03 13:51:08 -0800

    Commit and Push

--- slurm/final/final_clean_adapter_20_2.sh
+++ slurm/final/final_clean_adapter_20_2.sh
@@ -3,11 +3,11 @@
 #SBATCH -n 1
 #SBATCH --gres gpu:A100:1
 
-#SBATCH --error=error_final_clean_adapter_20_resume.log
-#SBATCH --output=output_final_clean_adapter_20_resume.log
+#SBATCH --error=error_final_clean_adapter_20.log
+#SBATCH --output=output_final_clean_adapter_20.log
 
 #SBATCH --time 2-00:00:00 #Time for the job to run
-#SBATCH --job-name RCA20
+#SBATCH --job-name ClnAdp20
 
 ##SBATCH -p requeue
 #SBATCH -p gpu
@@ -23,4 +23,4 @@ source activate /home/yz3qt/data/miniconda/envs/splm
 export TORCH_HOME=/home/yz3qt/data/torch_cache/
 export HF_HOME=/home/yz3qt/data/transformers_cache/
 
-python ./run.py --config_path ./config/final/final_clean_adapter_20.yaml --mode train --resume_path ./result/final_clean/20241125-02-08-58/checkpoint/model_triplet.pth --result_path ./result/final_clean_adapter_20_resume/ || true
+python ./run.py --config_path ./config/final/final_clean_adapter_20_2.yaml --resume_path ./result/final_clean/20241201-17-25-29/checkpoint/model_triplet.pth --mode train --result_path ./result/final_clean/ || true
